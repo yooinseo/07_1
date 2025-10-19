0a9867940da7c1c172f4a9bc66696c1a04758496
yooinseo <qetu37@sookmyung.ac.kr>
2025-10-19 18:31:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-int inc(int counter);
+void f(void);
 
 int main(int argc, char *argv[])
 {
-   int i=10;
-   
-   printf("함수 호출전 i=%d\n", i);
-   i=inc(i);
-   printf("함수 호출후 i=%d\n", i); 
+   int i;
+   for (i=0;i<5;i++)
+   {
+        f();
+   }            
    
   system("PAUSE");	
   return 0;
 }
 
-int inc(int counter)
+void f(void)
 {
-    counter++;
-    return counter; 
+    int i;
+    for (i=0;i<10;i++)
+        printf("#");
 }
