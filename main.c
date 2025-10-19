5497e1d67d12f44adefc3995c095a44f7276bc10
yooinseo <qetu37@sookmyung.ac.kr>
2025-10-19 15:45:53 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+   int flag=1;
+    int y;
+    while (flag=!0)
+    {
+          int y;
+          y=3;
+          flag = 0;
+    }
+    y=4;
+  system("PAUSE");	
+  return 0;
+}
