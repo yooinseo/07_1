43a0883bece3df91a2986f8da0a449dfacc7ac3e
yooinseo <qetu37@sookmyung.ac.kr>
2025-10-19 19:34:37 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-void f(void);
+void sub(void);
 
 int main(int argc, char *argv[])
 {
    int i;
-   for (i=0;i<5;i++)
-   {
-        f();
-   }            
-   
+   for (i=0;i<3;i++)
+     sub();
+
   system("PAUSE");	
   return 0;
 }
 
-void f(void)
+void sub(void)
 {
-    int i;
-    for (i=0;i<10;i++)
-        printf("#");
+    int auto_count=0;
+    static int static_count=0;
+    auto_count++;
+    static_count++;
+    printf("auto_count=%d\n",auto_count);
+    printf("static_count=%d\n", static_count);
 }
