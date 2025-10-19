e75eff53ba03b2fd01b2ec77b027d58500e90024
yooinseo <qetu37@sookmyung.ac.kr>
2025-10-19 19:50:20 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-void sub(void);
+int all_files;
+
+extern void sub(void);
 
 int main(int argc, char *argv[])
 {
-   int i;
-   for (i=0;i<3;i++)
-     sub();
+   sub();
+   printf("%d\n", all_files);
 
   system("PAUSE");	
   return 0;
-}
-
-void sub(void)
-{
-    int auto_count=0;
-    static int static_count=0;
-    auto_count++;
-    static_count++;
-    printf("auto_count=%d\n",auto_count);
-    printf("static_count=%d\n", static_count);
 }
